82d446ca68ea2fa5ca37c00f6d0438f1e94d010c
Nur Latifah Ulfah <n.latifahulfah31@gmail.com>
2017-07-27 12:07:49 +0700

    home

--- resources/views/complaints/ad-home.blade.php
+++ resources/views/complaints/ad-home.blade.php
@@ -5,47 +5,77 @@
 
 @section('content')
 
+	<div class="chart-container">
+		<canvas id="chart"></canvas>
+	</div>
+	<div class="row">
+		<div class="col l5"></div>
+		<div class="input-field col l2">
+		    <select>
+		      	<option value="1" selected>Filter 1</option>
+		      	<option value="2">Filter 2</option>
+		      	<option value="3">Filter 3</option>
+		    </select>
+		    <label>Filter by</label>
+		</div>
+	</div>
+	
+
+	<div class="row">
+		<div class="col s12 l3"></div>
+		<div class="col s12 l2  jumlah">
+			<div class="row counter-text">68</div>
+			<div class="row red darken-2 white-text">New Complaints</div>
+		</div>	
+		<div class="col s12 l2  jumlah">
+			<div class="row counter-text">80</div>
+			<div class="row amber white-text">On Review Complaints</div>
+		</div>	
+		<div class="col s12 l2  jumlah">
+			<div class="row counter-text">109</div>
+			<div class="row green darken-3 white-text">Answered Complaints</div>
+		</div>	
+	</div>
+
+	<script src="js/Chart.bundle.min.js"></script>
+	<script>
+	var ctx = document.getElementById('chart').getContext('2d');
+
+	var vlabels = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"];
+	var vdata = [0, 10, 5, 2, 20, 30, 45];
+	var chart = new Chart(ctx, {
+	    // The type of chart we want to create
+	    type: 'line',
+
+	    // The data for our dataset
+	    data: {
+	        labels: vlabels,
+	        datasets: [{
+	            label: "Total Complaints",
+	            data: vdata,
+	            backgroundColor: 'rgba(255, 255, 255, 0)',
+	            borderColor: 'rgb(255, 99, 132)',
+	            lineTension: 0,
+	        }]
+	    },
+
+	    // Configuration options go here
+	    options: {
+	    	legend: {
+	            display: true,
+	            labels: {
+	                fontColor: 'rgb(255, 99, 132)'
+	            }
+	        },
+
+	    }
+	});
+
+	
+	var vlabels = ["2016","2017"];
+	var vdata = [40,50];
+
+	</script>
 
 @endsection
-	<!--
-	<div class="row" style="margin-bottom: 0px;">-->
-		<!-- Sidebar  --><!--
-		<div class="col s12 m4 l2 " style="padding: 0px;">
-			<ul class="collapsible " style="margin:0px; height: 90vh;" data-collapsible="accordion">
-				<li>
-					<a class="collapsible-header" href="{{route('homeadmin')}}">Home</a>
-				</li>
-				<li>
-					<a class="collapsible-header" href="{{route('statistic')}}">Statistic</a>
-				</li>
-				<li>
-					<a class="collapsible-header" href="{{route('viewcomplaint')}}">Complaint</a>
-				</li>
-				<li>
-					<a class="collapsible-header" href="#">Categories<i class="material-icons right">keyboard_arrow_down</i></a>
-					<div class="collapsible-body" style="padding: 1rem 1rem 1rem 2rem;">
-						<a href="{{route('createkategori')}}"><i class="material-icons">add_circle_outline</i>New Category</a>
-					</div>
-					<div class="collapsible-body" style="padding: 1rem 1rem 1rem 2rem;">
-						<a href="{{route('editkategori')}}"><i class="material-icons">create</i>Edit Category</a>
-					</div>
-				</li>
-				<li>
-					<a class="collapsible-header" href="{{route('regisfreelancer')}}">Freelancer</a>
-				</li>
-				<li>
-					<a class="collapsible-header" href="{{ route('logout') }}"
-					   onclick="event.preventDefault();document.getElementById('logout-form').submit();">
-						Logout
-					</a>
-					<form id="logout-form" action="{{ route('logout') }}" method="POST" >
-						{{ csrf_field() }}
-					</form>
-				</li>
-			</ul>
-		</div> -->
-		<!-- Content --> <!--
-		<div class="col s12 m8 l10 blue-grey lighten-5" style="height: 90vh;">
-		
-		</div> 
-	</div> -->
+	
